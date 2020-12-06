e2434ec832cd03b1e30c713575e0d1cea78c3bc9
aelmessaarab <aelmessaarab@gmail.com>
2020-12-06 20:03:50 +0100

    Added main site

--- main.js
+++ main.js
@@ -141,6 +141,7 @@ app.get("/plane", function (req, res) {
     console.log(airplane);
     io.emit("planedata", airplane);
 });
+app.use("/", express.static('/home/aelmessaarab/tdr_omsintheair/server/app'));
 app.get("*", function (req, res) {
     res.json({
         "ERROR": 404
