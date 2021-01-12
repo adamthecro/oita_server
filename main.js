e444c2cd4532311673493e234831aeda7dd69f28
Adam El Messaoudi <aelmessaarab@gmail.com>
2021-01-12 13:27:46 +0100

    Improvements

--- main.js
+++ main.js
@@ -4,20 +4,21 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 
+
 var airplane = {
     "pi": 0,
     "local_time": 0,
 
-    "m0": 0,
-    "m1": 0,
+    "m_l": 0,
+    "m_r": 0,
 
     "s0": 0,
     "s1": 0,
     "s2": 0,
     "s3": 0,
     "s4": 0,
-    "s5": 0,
-    "s6": 0,
+    "s_l": 0,
+    "s_r": 0,
 
     "accx_m1": 0,
     "accy_m1": 0,
@@ -65,77 +66,87 @@ var airplane = {
 };
 
 var client = {
-    "phase": 0,
-    "m1": 0,
-    "m2": 0,
-    "s1": 0,
-    "s2": 0,
-    "s3": 0,
-    "s4": 0,
-    "s5": 0,
-    "s6": 0,
-    "s7": 0,
+    "mode": 0,  //0
+    "m_l": 0,   //1
+    "m_r": 0,   //2
+    "s0": 0,    //3
+    "s1": 0,    //4
+    "s2": 0,    //5
+    "s3": 0,    //6
+    "s4": 0,    //7
+    "s_l": 0,   //8
+    "s_r": 0,   //9
 };
 
-app.get("/plane", function (req, res) {
-    res.json(client);
+apps.get("/plane", function (req, res) {
+    res.send(client.mode + "," +
+        client.m_l + "," +
+        client.m_r + "," +
+        client.s0 + "," +
+        client.s1 + "," +
+        client.s2 + "," +
+        client.s3 + "," +
+        client.s4 + "," +
+        client.s_l + "," +
+        client.s_r + ",");
     res.end();
-    airplane["pi"] = req.query.pi;
+    var data = req._parsedUrl.query.split(";");
+    airplane["pi"] = data[0];
 
-    airplane["m0"] = req.query.m0;
-    airplane["m1"] = req.query.m1;
+    airplane["m_l"] = data[8];
+    airplane["m_r"] = data[9];
 
-    airplane["s0"] = req.query.s0;
-    airplane["s1"] = req.query.s1;
-    airplane["s2"] = req.query.s2;
-    airplane["s3"] = req.query.s3;
-    airplane["s4"] = req.query.s4;
-    airplane["s5"] = req.query.s5;
-    airplane["s6"] = req.query.s6;
+    airplane["s0"] = data[10];
+    airplane["s1"] = data[11];
+    airplane["s2"] = data[12];
+    airplane["s3"] = data[13];
+    airplane["s4"] = data[14];
+    airplane["s_l"] = data[15];
+    airplane["s_r"] = data[16];
 
-    airplane["accx_m1"] = req.query.accx_m1;
-    airplane["accy_m1"] = req.query.accy_m1;
-    airplane["accz_m1"] = req.query.accz_m1;
-    airplane["gyrox_m1"] = req.query.gyrox_m1;
-    airplane["gyroy_m1"] = req.query.gyroy_m1;
-    airplane["gyroz_m1"] = req.query.gyroz_m1;
-    airplane["pitch_m1"] = req.query.pitch_m1;
-    airplane["roll_m1"] = req.query.roll_m1;
-    airplane["yaw_m1"] = req.query.yaw_m1;
+    airplane["accx_m1"] = data[17];
+    airplane["accy_m1"] = data[18];
+    airplane["accz_m1"] = data[19];
+    airplane["gyrox_m1"] = data[20];
+    airplane["gyroy_m1"] = data[21];
+    airplane["gyroz_m1"] = data[22];
+    airplane["pitch_m1"] = data[23];
+    airplane["roll_m1"] = data[24];
+    airplane["yaw_m1"] = data[25];
 
-    airplane["accx_m2"] = req.query.accx_m2;
-    airplane["accy_m2"] = req.query.accy_m2;
-    airplane["accz_m2"] = req.query.accz_m2;
-    airplane["gyrox_m2"] = req.query.gyrox_m2;
-    airplane["gyroy_m2"] = req.query.gyroy_m2;
-    airplane["gyroz_m2"] = req.query.gyroz_m2;
-    airplane["pitch_m2"] = req.query.pitch_m2;
-    airplane["roll_m2"] = req.query.roll_m2;
-    airplane["yaw_m2"] = req.query.yaw_m2;
+    airplane["accx_m2"] = data[26];
+    airplane["accy_m2"] = data[27];
+    airplane["accz_m2"] = data[28];
+    airplane["gyrox_m2"] = data[29];
+    airplane["gyroy_m2"] = data[30];
+    airplane["gyroz_m2"] = data[31];
+    airplane["pitch_m2"] = data[32];
+    airplane["roll_m2"] = data[33];
+    airplane["yaw_m2"] = data[34];
 
-    airplane["accx_m3"] = req.query.accx_m3;
-    airplane["accy_m3"] = req.query.accy_m3;
-    airplane["accz_m3"] = req.query.accz_m3;
-    airplane["gyrox_m3"] = req.query.gyrox_m3;
-    airplane["gyroy_m3"] = req.query.gyroy_m3;
-    airplane["gyroz_m3"] = req.query.gyroz_m3;
-    airplane["magx_m3"] = req.query.magx_m3;
-    airplane["magy_m3"] = req.query.magy_m3;
-    airplane["magz_m3"] = req.query.magz_m3;
-    airplane["pitch_m3"] = req.query.pitch_m3;
-    airplane["roll_m3"] = req.query.roll_m3;
-    airplane["yaw_m3"] = req.query.yaw_m3;
+    airplane["accx_m3"] = data[35];
+    airplane["accy_m3"] = data[36];
+    airplane["accz_m3"] = data[37];
+    airplane["gyrox_m3"] = data[38];
+    airplane["gyroy_m3"] = data[39];
+    airplane["gyroz_m3"] = data[40];
+    airplane["magx_m3"] = data[41];
+    airplane["magy_m3"] = data[42];
+    airplane["magz_m3"] = data[43];
+    airplane["pitch_m3"] = data[44];
+    airplane["roll_m3"] = data[45];
+    airplane["yaw_m3"] = data[46];
 
-    airplane["pitch_gi"] = req.query.pitch_gi;
-    airplane["roll_gi"] = req.query.roll_gi;
-    airplane["yaw_gi"] = req.query.yaw_gi;
+    airplane["pitch_gi"] = data[47];
+    airplane["roll_gi"] = data[48];
+    airplane["yaw_gi"] = data[49];
 
-    airplane["lat"] = req.query.lat;
-    airplane["long"] = req.query.long;
-    airplane["alt"] = req.query.alt;
-    airplane["speed"] = req.query.speed;
-    airplane["sats_used"] = req.query.sats_used;
-    airplane["gps_state"] = req.query.gps_state;
+    airplane["lat"] = data[2];
+    airplane["long"] = data[3];
+    airplane["alt"] = data[4];
+    airplane["speed"] = data[5];
+    airplane["sats_used"] = data[6];
+    airplane["gps_state"] = data[7];
 
 
     console.log("Received packet: " + airplane["pi"]);
@@ -149,10 +160,16 @@ server.listen(process.env.PORT || 80);
 io.on("connect", function (socket) {
     console.log("New socket connection: " + socket.id);
     socket.on("data", function (data) {
-
+        client = data;
     });
 
     socket.on("disconnect", function () {
         console.log("Disconnected: " + socket.id);
     });
+    socket.on("datatoplane", function (data) {
+        client = data;
+    });
+    socket.on("commbuts", function (data) {
+        client["mode"] = data;
+    })
 });
